9b35f41fc967c1e48dd1d6cfec7d4a862212c563
3fxcf9 <123954477+3fxcf9@users.noreply.github.com>
2023-06-13 16:12:54 +0200

    Better coords conversion system

--- main.js
+++ main.js
@@ -7,15 +7,81 @@ const line_url = `https://api.tisseo.fr/v2/lines.json?shortName={NAME}&displayGe
 const stops_url = `https://api.tisseo.fr/v2/stop_areas.json?lineId={ID}&displayCoordXY=1&key=${key}`;
 // Config end
 
+// DOM Elements
 const line_input = document.querySelector("input[name=line]");
 const submit_btn = document.querySelector("input[type=submit]");
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-line_input.addEventListener("keypress", (e) => {
-	if (e.key == "Enter") submit_btn.click();
-});
+// Utilities
+/**
+ * Convert SRID coords to canvas coords
+ */
+class CoordsConverter {
+	constructor() {
+		this.boundings = [
+			[Infinity, Infinity],
+			[0, 0],
+		];
+	}
+	reset() {
+		this.boundings = [
+			[Infinity, Infinity],
+			[0, 0],
+		];
+	}
+	updateBounding(coordinates) {
+		this.boundings[0][0] = Math.min(this.boundings[0][0], ...coordinates.map((coord) => coord[0]));
+		this.boundings[1][0] = Math.max(this.boundings[1][0], ...coordinates.map((coord) => coord[0]));
+		this.boundings[0][1] = Math.min(this.boundings[0][1], ...coordinates.map((coord) => coord[1]));
+		this.boundings[1][1] = Math.max(this.boundings[1][1], ...coordinates.map((coord) => coord[1]));
+	}
+	convertCoordinatesToPixels(coords) {
+		const [lon, lat] = coords;
+		const x = (lon - this.boundings[0][0]) * ((canvas.width - 100) / (this.boundings[1][0] - this.boundings[0][0])) + 50;
+		const y = canvas.height - 100 - (lat - this.boundings[0][1]) * ((canvas.height - 100) / (this.boundings[1][1] - this.boundings[0][1])) + 50;
+		// Without margin
+		// const x = (lon - this.boundings[0][0]) * (canvas.width / (this.boundings[1][0] - this.boundings[0][0])) + 10;
+		// const y = canvas.height - (lat - this.boundings[0][1]) * (canvas.height / (this.boundings[1][1] - this.boundings[0][1]));
+		return [x, y];
+	}
+}
+/**
+ * Find the nearest point in path
+ */
+class knn_points {
+	/**
+	 * Knn points constructor
+	 * @param {[number, number][]} points
+	 */
+	constructor(points) {
+		self.points = points;
+	}
+
+	/**
+	 * Closest point to stop
+	 * @param {{x:number, y:number}} stop
+	 * @returns {{x:number, y:number}}
+	 */
+	closest(stop) {
+		let min_dist = Infinity;
+		let best_point = stop;
+
+		self.points.forEach((point) => {
+			const distance = Math.sqrt(Math.pow(point[0] - stop[0], 2) + Math.pow(point[1] - stop[1], 2));
+
+			if (distance < min_dist) {
+				min_dist = distance;
+				best_point = point;
+			}
+		});
+
+		return { x: best_point[0], y: best_point[1] };
+	}
+}
+
+const coordsConverter = new CoordsConverter();
 
 submit_btn.addEventListener("click", (e) => {
 	axios
@@ -24,29 +90,20 @@ submit_btn.addEventListener("click", (e) => {
 			const line = response.data.lines.line[0];
 			const geodata = parse(line.geometry[0].wkt);
 
-			minLon = Infinity;
-			maxLon = 0;
-			minLat = Infinity;
-			maxLat = 0;
+			coordsConverter.reset(); // Reset conversion bounding box
+			clear(); // Clear canvas
 
 			geodata.geometries.forEach((geometry) => {
-				const coordinates = geometry.coordinates;
-
-				// Update box
-				minLon = Math.min(minLon, ...coordinates.map((coord) => coord[0]));
-				maxLon = Math.max(maxLon, ...coordinates.map((coord) => coord[0]));
-				minLat = Math.min(minLat, ...coordinates.map((coord) => coord[1]));
-				maxLat = Math.max(maxLat, ...coordinates.map((coord) => coord[1]));
+				coordsConverter.updateBounding(geometry.coordinates);
 			});
 
-			clear();
 			console.log("Number of geometries: ", geodata.geometries.length);
-			const allPoints = [];
+
+			const allPoints = []; // Used in knn
 			geodata.geometries.forEach((geometry) => {
 				const coordinates = geometry.coordinates;
-				console.log(minLon, maxLon, minLat, maxLat);
 
-				const pixelCoordinates = coordinates.map((coord) => convertCoordinatesToPixels(coord));
+				const pixelCoordinates = coordinates.map((coord) => coordsConverter.convertCoordinatesToPixels(coord));
 				allPoints.push.apply(allPoints, pixelCoordinates);
 
 				plotLine(pixelCoordinates, line.bgXmlColor);
@@ -57,7 +114,7 @@ submit_btn.addEventListener("click", (e) => {
 			axios.get(stops_url.replace("{ID}", line.id)).then((response) => {
 				const stops = response.data.stopAreas.stopArea;
 				stops.forEach((stop) => {
-					const closest = knn.closest(convertCoordinatesToPixels([stop.x, stop.y]));
+					const closest = knn.closest(coordsConverter.convertCoordinatesToPixels([stop.x, stop.y]));
 					drawPoint(closest.x, closest.y, stop.name, line.bgXmlColor);
 				});
 			});
@@ -70,21 +127,6 @@ submit_btn.addEventListener("click", (e) => {
 function clear() {
 	ctx.fillStyle = "#121212";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
-
-let minLon = Infinity;
-let maxLon = 0;
-let minLat = Infinity;
-let maxLat = 0;
-
-function convertCoordinatesToPixels(coords) {
-	const [lon, lat] = coords;
-	const x = (lon - minLon) * ((canvas.width - 100) / (maxLon - minLon)) + 50;
-	const y = canvas.height - 100 - (lat - minLat) * ((canvas.height - 100) / (maxLat - minLat)) + 50;
-	// Without margin
-	// const x = (lon - minLon) * (canvas.width / (maxLon - minLon)) + 10;
-	// const y = canvas.height - (lat - minLat) * (canvas.height / (maxLat - minLat));
-	return [x, y];
 }
 
 function drawPoint(x, y, name, color) {
@@ -123,34 +165,3 @@ const side = Math.min(
 );
 canvas.height = side;
 canvas.width = side;
-
-class knn_points {
-	/**
-	 * Knn points constructor
-	 * @param {[number, number][]} points
-	 */
-	constructor(points) {
-		self.points = points;
-	}
-
-	/**
-	 * Closest point to stop
-	 * @param {{x:number, y:number}} stop
-	 * @returns {{x:number, y:number}}
-	 */
-	closest(stop) {
-		let min_dist = Infinity;
-		let best_point = stop;
-
-		self.points.forEach((point) => {
-			const distance = Math.sqrt(Math.pow(point[0] - stop[0], 2) + Math.pow(point[1] - stop[1], 2));
-
-			if (distance < min_dist) {
-				min_dist = distance;
-				best_point = point;
-			}
-		});
-
-		return { x: best_point[0], y: best_point[1] };
-	}
-}
